e63cdd77ff50c8b616bb3ef07b555bfe0315a6b7
burche1 <burche.t.d@gmail.com>
2018-09-26 15:11:57 -0300

    Update sagui_example.c

--- External Interrupt/sagui_example.c
+++ External Interrupt/sagui_example.c
@@ -6,6 +6,7 @@
 #include "stm32f10x_gpio.h"
 #include "stm32f10x_rcc.h"
 #include "stm32f10x_exti.h"
+#include "stm32f10x_tim.h"
 
 /*****************************************************************************************************/
 
