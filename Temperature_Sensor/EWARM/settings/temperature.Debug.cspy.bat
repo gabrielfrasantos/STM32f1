b6de5901d77a8f703f56294d3e96113c472e8219
burche1 <burche.t.d@gmail.com>
2018-10-14 14:10:52 -0300

    temperature adc

--- Temperature_Sensor/EWARM/settings/temperature.Debug.cspy.bat
+++ Temperature_Sensor/EWARM/settings/temperature.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.1\common\bin\cspybat" -f "C:\Users\Alexandre\Desktop\Temperature_Sensor\EWARM\settings\temperature.Debug.general.xcl" --backend -f "C:\Users\Alexandre\Desktop\Temperature_Sensor\EWARM\settings\temperature.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.1\common\bin\cspybat" -f "C:\Users\Alexandre\Documents\Git_Embedded\STM32f1\Temperature_Sensor\EWARM\settings\temperature.Debug.general.xcl" --backend -f "C:\Users\Alexandre\Documents\Git_Embedded\STM32f1\Temperature_Sensor\EWARM\settings\temperature.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.1\common\bin\cspybat" -f "C:\Users\Alexandre\Desktop\Temperature_Sensor\EWARM\settings\temperature.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\Alexandre\Desktop\Temperature_Sensor\EWARM\settings\temperature.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.1\common\bin\cspybat" -f "C:\Users\Alexandre\Documents\Git_Embedded\STM32f1\Temperature_Sensor\EWARM\settings\temperature.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\Alexandre\Documents\Git_Embedded\STM32f1\Temperature_Sensor\EWARM\settings\temperature.Debug.driver.xcl" 
 
 @echo off 
 :end
--- Temperature_Sensor/EWARM/settings/temperature.Debug.cspy.ps1
+++ Temperature_Sensor/EWARM/settings/temperature.Debug.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.1\common\bin\cspybat" -f "C:\Users\Alexandre\Desktop\Temperature_Sensor\EWARM\settings\temperature.Debug.general.xcl" --backend -f "C:\Users\Alexandre\Desktop\Temperature_Sensor\EWARM\settings\temperature.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.1\common\bin\cspybat" -f "C:\Users\Alexandre\Documents\Git_Embedded\STM32f1\Temperature_Sensor\EWARM\settings\temperature.Debug.general.xcl" --backend -f "C:\Users\Alexandre\Documents\Git_Embedded\STM32f1\Temperature_Sensor\EWARM\settings\temperature.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.1\common\bin\cspybat" -f "C:\Users\Alexandre\Desktop\Temperature_Sensor\EWARM\settings\temperature.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\Alexandre\Desktop\Temperature_Sensor\EWARM\settings\temperature.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.1\common\bin\cspybat" -f "C:\Users\Alexandre\Documents\Git_Embedded\STM32f1\Temperature_Sensor\EWARM\settings\temperature.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\Alexandre\Documents\Git_Embedded\STM32f1\Temperature_Sensor\EWARM\settings\temperature.Debug.driver.xcl" 
 }
