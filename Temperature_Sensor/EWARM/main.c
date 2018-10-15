afc6cd03ac4a17774b70e6db5fdb01b14724b212
burche1 <burche.t.d@gmail.com>
2018-10-15 15:04:33 -0300

    taking USART out

--- Temperature_Sensor/EWARM/main.c
+++ Temperature_Sensor/EWARM/main.c
@@ -1,76 +1,78 @@
 #include "stm32f10x.h"
 #include "stm32f10x_gpio.h"
 #include "stm32f10x_rcc.h"
-#include "stm32f10x_usart.h"
+//#include "stm32f10x_usart.h"
 #include "stm32f10x_adc.h"
 #include "stdio.h"
 #include "misc.h"
 
-void usart_init(void)
-{
-        /* Enable USART1 and GPIOA clock */
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1 | RCC_APB2Periph_GPIOA, ENABLE);
 
-	/* NVIC Configuration */
-	NVIC_InitTypeDef NVIC_InitStructure;
-	/* Enable the USARTx Interrupt */
-	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_Init(&NVIC_InitStructure);
+//void usart_init(void)
+//{
+//        /* Enable USART1 and GPIOA clock */
+//	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1 | RCC_APB2Periph_GPIOA, ENABLE);
+//
+//	/* NVIC Configuration */
+//	NVIC_InitTypeDef NVIC_InitStructure;
+//	/* Enable the USARTx Interrupt */
+//	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+//	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+//	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+//	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+//	NVIC_Init(&NVIC_InitStructure);
+//
+//	/* Configure the GPIOs */
+//	//GPIO_Configuration();
+//	GPIO_InitTypeDef GPIO_InitStructure;
+//
+//	/* Configure USART1 Tx (PA.09) as alternate function push-pull */
+//	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+//	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+//	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+//	GPIO_Init(GPIOA, &GPIO_InitStructure);
+//
+//	/* Configure USART1 Rx (PA.10) as input floating */
+//	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+//	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+//	GPIO_Init(GPIOA, &GPIO_InitStructure);
+//
+//	/* Configure the USART1 */
+//	//USART_Configuration();
+//	USART_InitTypeDef USART_InitStructure;
+//
+//	/* USART1 configuration ------------------------------------------------------*/
+//	/* USART1 configured as follow:
+//	          - BaudRate = 115200 baud
+//	          - Word Length = 8 Bits
+//	          - One Stop Bit
+//	          - No parity
+//	          - Hardware flow control disabled (RTS and CTS signals)
+//	          - Receive and transmit enabled
+//	          - USART Clock disabled
+//	          - USART CPOL: Clock is active low
+//	          - USART CPHA: Data is captured on the middle
+//	          - USART LastBit: The clock pulse of the last data bit is not output to
+//	                           the SCLK pin
+//	 */
+//	USART_InitStructure.USART_BaudRate = 115200;
+//	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+//	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+//	USART_InitStructure.USART_Parity = USART_Parity_No;
+//	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+//	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+//
+//	USART_Init(USART1, &USART_InitStructure);
+//
+//	/* Enable USART1 */
+//	USART_Cmd(USART1, ENABLE);
+//
+//	/* Enable the USART1 Receive interrupt: this interrupt is generated when the
+//		USART1 receive data register is not empty */
+//	//USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+//
+//}
 
-	/* Configure the GPIOs */
-	//GPIO_Configuration();
-	GPIO_InitTypeDef GPIO_InitStructure;
-
-	/* Configure USART1 Tx (PA.09) as alternate function push-pull */
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-	/* Configure USART1 Rx (PA.10) as input floating */
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-	/* Configure the USART1 */
-	//USART_Configuration();
-	USART_InitTypeDef USART_InitStructure;
-
-	/* USART1 configuration ------------------------------------------------------*/
-	/* USART1 configured as follow:
-	          - BaudRate = 115200 baud
-	          - Word Length = 8 Bits
-	          - One Stop Bit
-	          - No parity
-	          - Hardware flow control disabled (RTS and CTS signals)
-	          - Receive and transmit enabled
-	          - USART Clock disabled
-	          - USART CPOL: Clock is active low
-	          - USART CPHA: Data is captured on the middle
-	          - USART LastBit: The clock pulse of the last data bit is not output to
-	                           the SCLK pin
-	 */
-	USART_InitStructure.USART_BaudRate = 115200;
-	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-	USART_InitStructure.USART_StopBits = USART_StopBits_1;
-	USART_InitStructure.USART_Parity = USART_Parity_No;
-	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-
-	USART_Init(USART1, &USART_InitStructure);
-
-	/* Enable USART1 */
-	USART_Cmd(USART1, ENABLE);
-
-	/* Enable the USART1 Receive interrupt: this interrupt is generated when the
-		USART1 receive data register is not empty */
-	//USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
-
-}
-
+/*
 void USARTSend(char *pucBuffer, unsigned long ulCount)
 {
     //
@@ -79,12 +81,13 @@ void USARTSend(char *pucBuffer, unsigned long ulCount)
     while(ulCount--)
     {
         USART_SendData(USART1, *pucBuffer++);// Last Version USART_SendData(USART1,(uint16_t) *pucBuffer++);
-        /* Loop until the end of transmission */
+         //Loop until the end of transmission 
         while(USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET)
         {
         }
     }
 }
+*/
 
 void SetSysClockTo72(void)
 {
@@ -154,8 +157,8 @@ int main(void)
 	SetSysClockTo72();
 
 	//USART1
-	usart_init();
-	USART_SendData(USART1, '\r');
+	//usart_init();
+	//USART_SendData(USART1, '\r');
 
 	//ADC
 	ADC_InitTypeDef ADC_InitStructure;
@@ -197,7 +200,7 @@ int main(void)
 	while (1)
 	{
 		adc_value = ADC_GetConversionValue(ADC1);
-		sprintf(buffer, "%d\r\n", adc_value);
-		USARTSend(buffer, sizeof(buffer));
+		//sprintf(buffer, "%d\r\n", adc_value);
+		//USARTSend(buffer, sizeof(buffer));
 	}
 }
